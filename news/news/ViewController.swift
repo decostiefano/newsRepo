f13769aa3da936a840c98faa76aa99ef7ade4993
decostiefano <decostfn@gmail.com>
2022-09-08 17:43:45 +0700

    first commit

---
+++ news/news/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  news
+//
+//  Created by Jerry Purnama Maulid on 08/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
